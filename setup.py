98d8852db2ee8802802d26751db96c5067f8a750
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-12-19 11:53:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='parisneo-precommit-hooks',
+    version='0.1.0',
+    packages=find_packages(),
+    author='ParisNeo',
+    author_email='parisneo@parisneo.fr',
+    description='ParisNeo\'s Custom Pre-commit Hooks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ParisNeo/parisneo-precommit-hooks',
+    install_requires=[
+        'black',
+        'flake8',
+        'isort',
+        'pylint',
+        'eslint',
+        'prettier'
+    ],
+    entry_points={
+        'console_scripts': [
+            'parisneo-python-check=parisneo_hooks.python_hooks:python_style_check',
+            'parisneo-js-check=parisneo_hooks.js_hooks:js_style_check',
+        ],
+    },
+)
