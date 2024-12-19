04eeb5ab0e3f6305cd34945d40e2e6e56c11ba42
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-12-19 12:02:13 +0100

    removed test and updated setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     author='ParisNeo',
-    author_email='parisneo@parisneo.fr',
+    author_email='parisneo@gmail.com',
     description='ParisNeo\'s Custom Pre-commit Hooks',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
